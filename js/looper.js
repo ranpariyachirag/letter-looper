297390495cf284ad6d7bbda9b7c70cba3e3cfee2
Chirag Ranpariya <ranp0004@algonquinlive.com>
2017-01-23 12:53:49 -0500

    Add files via upload

---
+++ js/looper.js
@@ -0,0 +1,22 @@
+var i,b,a,j;
+
+while(!a)
+    {
+       a = prompt("enter your character");
+     
+    }
+
+
+        for(i=1;i<11;i++)
+            {
+            document.write("<br>");
+                
+                for(j=0;j<i;j++)
+                    {
+                
+            
+        
+                document.write(a);
+                    }
+            }
+    
